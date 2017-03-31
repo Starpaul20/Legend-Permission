66bf0f3ce6167a3603b80508d331fc0da55aafe9
PaulBender <paulbender@live.com>
2017-03-31 12:04:31 -0400

    Use THIS_SCRIPT for template caching

--- inc/plugins/legendperm.php
+++ inc/plugins/legendperm.php
@@ -11,7 +11,7 @@ if(!defined("IN_MYBB"))
 }
 
 // Neat trick for caching our custom template(s)
-if(my_strpos($_SERVER['PHP_SELF'], 'editpost.php'))
+if(THIS_SCRIPT == 'editpost.php')
 {
 	global $templatelist;
 	if(isset($templatelist))
