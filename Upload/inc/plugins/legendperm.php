b5eb09844168f9027319c3e3c94e9f287cfff98d
Paul Bender <paulbender@live.com>
2024-05-10 15:17:16 -0400

    Bump to version v1.3

--- Upload/inc/plugins/legendperm.php
+++ Upload/inc/plugins/legendperm.php
@@ -44,7 +44,7 @@ function legendperm_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.3",
 		"codename"			=> "legendperm",
 		"compatibility"		=> "18*"
 	);
